7a6eacb027219d8fe938791ddeb23ba51111d3c4
mousse <guillaume.bergerot@etu.unicaen.fr>
2024-01-09 11:27:24 +0100

    created UserController and UserCredits component

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function remainingCredits()
+    {
+        // Replace with your actual SQL, make sure to use DB::raw() for raw expressions
+        $query = \DB::table('PARTICIPATE')
+        ->select('99-count(*) as remaining_credits')
+        ->join('DIVES', 'DIVES.div_id', '=', 'PARTICIPATE.div_id')
+        ->where('DVR_LICENCE','=',1004)
+        ->where(`DATEDIFF(div_date, STR_TO_DATE(CONCAT('01-01-',DATE_FORMAT(NOW(), '%Y')),'%d-%m-%Y'))`,'>=',0)
+        ->where(`DATEDIFF(div_date, STR_TO_DATE(CONCAT('01-01-',DATE_FORMAT(NOW(), '%Y')+1),'%d-%m-%Y'))`, '<', 0)
+        ->where('STA_ID','!=', 3);
+
+        return $query->get();
+    }
 }
