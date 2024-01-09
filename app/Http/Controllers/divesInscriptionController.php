7f2fb54ffda8e3510470c6772e38db6e54b0ed24
Maxence <levesqu221@campus.unicaen.fr>
2024-01-09 10:59:55 +0100

    Feature: Register in database

--- app/Http/Controllers/divesInscriptionController.php
+++ app/Http/Controllers/divesInscriptionController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Dive;
 
 class divesInscriptionController extends Controller
 {
-    //
+ 
+    public function registerDiverInTimeSlot(){
+        Dive::registerDiverInTimeSlot(1001);
+    }
+
 }
