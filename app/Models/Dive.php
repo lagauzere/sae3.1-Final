4df0e2d106a27c7d6b59fd7bf15766a5d45d9775
mousse <guillaume.bergerot@etu.unicaen.fr>
2024-01-10 10:28:07 +0100

    added examples

--- app/Models/Dive.php
+++ app/Models/Dive.php
@@ -23,6 +23,26 @@ class Dive extends Model
         join DIVING_LEVELS on (DIVING_LEVELS.DLV_ID = DIVES.DLV_ID)
         where STATUS.STA_ID = 1 and DIV_DATE > SYSDATE()');
     } 
+
+    //limited to 4 weeks
+    public function diveFinished(){
+        return DB::select('SELECT DIV_ID, SHP_NAME, STA_LABEL, SIT_NAME, DLV_DESC, DVR_NAME, DVR_FIRST_NAME, DIV_DATE, DLV_DESC FROM DIVES
+        join STATUS using (STA_ID)
+        join SITES using (SIT_ID)
+        join SHIPS using (SHP_ID)
+        join DIVERS on (DIVERS.DVR_LICENCE = DVR_LICENCE_DIRECTS)
+        join DIVING_LEVELS on (DIVING_LEVELS.DLV_ID = DIVES.DLV_ID)
+        where STATUS.STA_ID = 2 and DIV_DATE > SYSDATE()');
+    } 
+    public function diveCancelled(){
+        return DB::select('SELECT DIV_ID, SHP_NAME, STA_LABEL, SIT_NAME, DLV_DESC, DVR_NAME, DVR_FIRST_NAME, DIV_DATE, DLV_DESC FROM DIVES
+        join STATUS using (STA_ID)
+        join SITES using (SIT_ID)
+        join SHIPS using (SHP_ID)
+        join DIVERS on (DIVERS.DVR_LICENCE = DVR_LICENCE_DIRECTS)
+        join DIVING_LEVELS on (DIVING_LEVELS.DLV_ID = DIVES.DLV_ID)
+        where STATUS.STA_ID = 3 and DIV_DATE > SYSDATE()');
+    } 
     
     public function getDiversList($div_id){
         return DB:: select('select DVR_NAME,DVR_FIRST_NAME from DIVERS where DVR_LICENCE in (select DVR_LICENCE from PARTICIPATE where DIV_ID=?);',[$div_id]);
