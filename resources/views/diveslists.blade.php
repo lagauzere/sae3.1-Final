34e6ab3144ee5e392055b2682d0c24d5d93c6264
Cureau <baptiste.cureau@etu.unicaen.fr>
2024-01-10 13:25:24 +0100

    feat : register and cancel a participation

--- resources/views/diveslists.blade.php
+++ resources/views/diveslists.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=a, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Liste des plongées </title>
     @livewireStyles
 </head> 
@@ -14,71 +15,107 @@
     ?>
 
     <div>
-    <div id='calendar-container' wire:ignore>
-        <div id='calendar'></div>
+        <div id='calendar-container' wire:ignore>
+            <div id='calendar'></div>
+        </div>
     </div>
+
+    <div wire:ignore.self>
+        <div class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content" id="dynamic-modal-content">
+                    <!-- Le contenu de la modal sera injecté ici dynamiquement depuis JavaScript -->
+                </div>
+            </div>
+        </div>
     </div>
 
     @push('scripts')
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js'></script>
-<script>
-    document.addEventListener('livewire:load', function () {
-        
-        const Calendar = FullCalendar.Calendar;
-        var array = <?php echo $test?>;
-        console.table(array);
-        const calendarEl = document.getElementById('calendar');
-        
-        const divesData = <?php echo $test; ?>;
-        
-        const events = divesData.map((dive) => ({
-            title: "Plongée numéro: " + dive.DIV_ID,
-            start: new Date(dive.DIV_DATE), 
-            end: new Date(new Date(dive.DIV_DATE).getTime() + 3 * 60 * 60 * 1000),  
-            boat: `Bateau: ${dive.SHP_NAME}\n`,
-            site: `Site: ${dive.SIT_NAME}\n`,
-            requireLevel : `Niveau requis: ${dive.DLV_DESC}`,
-        }));
-        console.table(events);
-        const calendar = new Calendar(calendarEl, {
-            slotMinTime: '8:00:00',
-            slotMaxTime: '22:00:00',
-            initialView: 'timeGridWeek',
-            headerToolbar: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-            },
-            locale: 'fr',
-            events: events,
-            eventContent: function(info) {
+        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js'></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script>
+            document.addEventListener('livewire:load', function () {
+                const Calendar = FullCalendar.Calendar;
+
+                const calendarEl = document.getElementById('calendar');
+                
+                const divesData = <?php echo $test; ?>;
+                
+                const events = divesData.map((dive) => ({
+                    title: dive.DIV_ID,
+                    start: new Date(dive.DIV_DATE), 
+                    end: new Date(new Date(dive.DIV_DATE).getTime() + 3 * 60 * 60 * 1000),  
+                    boat: `Bateau: ${dive.SHP_NAME}\n`,
+                    site: `Site: ${dive.SIT_NAME}\n`,
+                    requireLevel : `Niveau requis: ${dive.DLV_DESC}`,
+                }));
+                const calendar = new Calendar(calendarEl, {
+                    slotMinTime: '8:00:00',
+                    slotMaxTime: '22:00:00',
+                    initialView: 'timeGridWeek',
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                    },
+                    locale: 'fr',
+                    events: events,
+                    eventContent: function(info) {
                         const content = document.createElement('div');
                         content.innerHTML = `
                             <div style="cursor: pointer;">
-                                <strong>${info.event.title}</strong><br>
+                                <strong>Plongée numéro:  ${info.event.title}</strong><br>
                                 <p>${info.event.extendedProps.boat}</p>
                                 <p>${info.event.extendedProps.site}</p>
                                 <p>${info.event.extendedProps.requireLevel}</p>
                             </div>
                         `;
+
                         return { domNodes: [content] };
                     },
-            eventClick: function(info) {
-                Livewire.emit('showEventRegistrationPopup', info.event.id);
-            }
-        });
-        calendar.render();
-    });
-</script>
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/locales-all.min.js"></script>
-<link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.css' rel='stylesheet' />
+                    eventClick: function(info) {
+                        const modalContent = `
+                            <div class="modal-header">
+                                <h5 class="modal-title"> Plongée numéro: ${info.event.title}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>${info.event.extendedProps.boat}</p>
+                                <p>${info.event.extendedProps.site}</p>
+                                <p>${info.event.extendedProps.requireLevel}</p>
+                            </div>
+                            <div class="modal-footer">
+                            <form action="{{ route('enterTimeSlot', ['selectedDive' => 1])}}" method="POST">
+                                @csrf
+                                <button type="submit" class="btn btn-primary">S'inscrire</button>
+                            </form>
+                            <form action="{{ route('leaveTimeSlot',['selectedDive' => 1]) }}" method="POST">
+                                @csrf 
+                                <button type="submit" class="btn btn-primary" data-dismiss="modal">Se désinscrire</button>
+                            </form>
+                            
 
-@endpush
+                            </div>
+
+                        `;
+
+                        document.getElementById('dynamic-modal-content').innerHTML = modalContent;
+
+                        $('.modal').modal('show');
+                    }
+                });
+                calendar.render();
+            });
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/locales-all.min.js"></script>
+        <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.css' rel='stylesheet' />
+    @endpush
 
     <livewire:calendar/>
-    <livewire:register-diver />
     @livewireScripts
     @stack('scripts')
-    
 </body>
 </html>
