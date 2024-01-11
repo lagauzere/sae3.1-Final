dec48b8ff99d9187065df565f6fb4663540d095a
Arthur Dubos <arthur.dubos@etu.unicaen.fr>
2024-01-11 13:29:25 +0100

    Display only ships with enough space

--- resources/views/diveParameters.blade.php
+++ resources/views/diveParameters.blade.php
@@ -26,7 +26,9 @@
     <button type="button" onclick="displayText('choiceBoat')">Modifier</button>  
         <select style="display:none" id="choiceBoat" name="choiceBoat" default="{{ $divesparameters[0]['SHP_NAME'] }}">
             @foreach($boatName as $boat)
+                @if(($divesparameters[0]["SHP_SEATS"] - $divesparameters[0]["DIV_HEADCOUNT"]) <= $boat['SHP_SEATS'])
                 <option value="{{ $boat['SHP_ID'] }}" @if($boat['SHP_NAME'] == $divesparameters[0]['SHP_NAME']) selected @endif>{{ $boat['SHP_NAME'] }} ({{ $boat['SHP_SEATS']}} places)</option>
+                @endif
             @endforeach
         </select>
     </h1>
