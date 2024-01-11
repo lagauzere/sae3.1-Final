1e42ecfdad759586c6e7f9470a3e3337eadb0851
Noe Tourbillon <tourbil221@campus.unicaen.fr>
2024-01-11 08:06:57 +0100

    fix : register popUp

--- resources/views/diveslists.blade.php
+++ resources/views/diveslists.blade.php
@@ -48,7 +48,9 @@
     </div>
 
     @push('scripts')
-   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js'>
+   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js'></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         </script>
         <script>
           
